303c4e0b10e544b60e6d880ae9cac4f4a3460c05
Can-Li <96718195+Can-Li@users.noreply.github.com>
2024-06-07 11:30:20 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import shutil
 
 
 def check_key():
-    file_name = f"key.txt"  # 替换为你希望创建的文件名
+    file_name = f"key.txt"  # 创建存放key的文件
 
     # 检查文件是否存在
     if not os.path.exists(file_name):
@@ -77,10 +77,25 @@ def get_path_decrypt_merge():
     # 列出目录下所有文件夹
     for folder_name in os.listdir(wx_location):
         if os.path.isdir(os.path.join(wx_location, folder_name)):
-            if 'wxid_' in folder_name:
+            if 'All Users' in folder_name or 'Applet' in folder_name or 'WMPF' in folder_name:
+                continue
+            # elif 'wxid_' in folder_name:
+            elif folder_name == '222':
                 contact_path = os.path.join(wx_location, folder_name, 'Msg')
                 msg_path = os.path.join(wx_location, folder_name, 'Msg', 'Multi')
+                if not os.path.exists(contact_path) and not os.path.exists(msg_path):
+                    print(f'文件夹不存在:{contact_path} {msg_path}')
+                    exit(0)
                 decrypt_db(contact_path, msg_path, folder_name)
+            else:
+                contact_path_input = input('未找到"wxid_"开头的目录,请手动输入文件夹名称:')
+                contact_path = os.path.join(wx_location, contact_path_input, 'Msg')
+                msg_path = os.path.join(wx_location, contact_path_input, 'Msg', 'Multi')
+                if not os.path.exists(contact_path) and not os.path.exists(msg_path):
+                    print(f'文件夹不存在:{contact_path} {msg_path}')
+                    exit(0)
+                decrypt_db(contact_path, msg_path, contact_path_input)
+                break
 
 
 def decrypt_db(contact_path, msg_path, user_path):
@@ -89,9 +104,9 @@ def decrypt_db(contact_path, msg_path, user_path):
     contact_micromsg_path = contact_path + '\\MicroMsg.db'
     dir_path = f'.\\db\\{user_path}'
     dir_micromsg_path = os.path.join(dir_path, 'MicroMsg.db')
-    check_dir_file(dir_path, dir_micromsg_path)
+    check_dir_file(dir_path, dir_micromsg_path)  # 检测文件文件夹是否存在，不存在则创建
 
-    decrypt(key, contact_micromsg_path, dir_micromsg_path)
+    decrypt(key, contact_micromsg_path, dir_micromsg_path)  # 解码数据库
     for folder_name in os.listdir(msg_path):
         if os.path.isfile(os.path.join(msg_path, folder_name)):
             if len(folder_name) == 7:
@@ -104,7 +119,7 @@ def decrypt_db(contact_path, msg_path, user_path):
     try:
         merge_db(user_path)  # 合并数据库
     except Exception as e:
-        print('非当前登录微信数据库,无法合并,自动跳过...')
+        print('非当前登录微信数据库,无法合并,跳过...')
 
 
 def read_all_files_in_directory(directory_path):
@@ -139,7 +154,7 @@ def merge_db(user_path):
     merge_databases([item for item in source_databases if 'MicroMsg.db' not in item and 'MSG0.db' not in item],
                     target_database)  # 合并数据库,列表推导式,排除MicroMsg.db、MSG0.db文件
     merge_table(f"db\\{user_path}\\MicroMsg.db", target_database, ['Contact', 'ChatRoom'])  # 将两个库文的表合成一个文件
-    remove_db(source_databases)  # 删除文件
+    # remove_db(source_databases)  # 删除文件
 
 
 def remove_db(file_paths):
@@ -174,17 +189,17 @@ if __name__ == '__main__':
     check_key()  # 检查存放key文件是否存在
     get_key()  # 获取key
     get_path_decrypt_merge()  # 自动获取路径，解密，合并数据库
+    wxids = get_wxid()
 
     while True:
         is_file = input('是否复制本地文件到当前路径?[复制后可在excel中超链接打开文件](y/n)')
-        wxids = get_wxid()
         if is_file == 'n':  # 不复制文件
             # 导出为excel
             for wx in wxids:  # 多个微信号,循环处理
                 try:
                     all_data = get_data(wx)
                 except Exception as e:
-                    print('当前处理数据库非当前登录微信,跳过...')
+                    print('当前处理数据库非当前登录微信,跳过...', e)
                     continue
                 deal_over_data = deal_data(all_data, wx)
                 write_excel(deal_over_data, wx)
@@ -195,7 +210,7 @@ if __name__ == '__main__':
                 try:
                     all_data = get_data(wx)
                 except Exception as e:
-                    print('当前处理数据库非当前登录微信,跳过...')
+                    print('当前处理数据库非当前登录微信,跳过...', e)
                     continue
                 print('正在复制本地文件到 .\\data\\files 目录下...')
                 for i in tqdm(range(len(all_data))):
